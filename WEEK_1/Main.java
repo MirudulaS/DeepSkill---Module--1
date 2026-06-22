759229810caff87e6320879e00144455e5cfa8ef
MIRUDULA S <mirudula.it@gmail.com>
2026-06-23 00:59:53 +0530

    Delete WEEK_1/Main.java

--- WEEK_1/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-	public static void main(String[] args) {
-		Finance finance = new Finance(10000, 1.10, 3);
-		double amount = finance.recursion(10000,1.10,3);
-		System.out.printf("Future Predicted Value: %.2f", amount);
-	}
-}
